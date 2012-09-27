537edf706a1aef27e9a33f528250b932ca7218d1
Jason Sanford <jason.sanford@mapmyfitness.com>
2012-09-27 00:02:31 -0600

    MapBox Terrain tiles

--- geojsonlint/static/js/app.js
+++ geojsonlint/static/js/app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         center: new L.LatLng(37.92686760148135, -96.767578125),
         zoom: 4,
         layers: [
-            new L.TileLayer('http://{s}.tiles.mapbox.com/v3/mapbox.mapbox-streets/{z}/{x}/{y}.png', {
+            new L.TileLayer('http://{s}.tiles.mapbox.com/v3/jcsanford.map-xu5k4lii/{z}/{x}/{y}.png', {
                 maxZoom: 16,
                 subdomains: ['a', 'b', 'c', 'd'],
                 attribution: 'Tiles Courtesy of <a href="http://www.mapbox.com/" target="_blank">MapBox</a>. Map data (c) <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors, CC-BY-SA.'
