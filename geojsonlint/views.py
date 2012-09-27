5963fa84d320a46fc07b7d849d65c1baf13f31d4
Jason Sanford <jason.sanford@mapmyfitness.com>
2012-09-27 14:20:00 -0600

    Checking if POSTed data is JSON parseable.

--- geojsonlint/views.py
+++ geojsonlint/views.py
@@ -23,7 +23,19 @@ def validate(request):
             message = 'GeoJSON data must be POSTed',
         )
         return HttpResponseBadRequest(json.dumps(resp), mimetype='application/json')
+    try:
+        test_geojson = json.loads(request.raw_post_data)
+    except json.JSONDecodeError as e:
+        return _geojson_error('POSTed data was not JSON parseable.')
+    # Everything checked out. Return 'ok'.
     resp = dict(
         status = 'ok',
     )
     return HttpResponse(json.dumps(resp), mimetype='application/json')
+
+def _geojson_error(message):
+    resp = dict(
+        status = 'error',
+        message = message,
+    )
+    return HttpResponse(resp, mimetype='application/json')
